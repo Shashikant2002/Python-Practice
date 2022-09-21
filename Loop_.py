c7bf8ba42bc4ff0fdc69886044fac200f4b139aa
Shashikant <shashikantprajapti17@gmail.com>
2022-09-21 12:54:29 +0530

    jfklsdjf

---
+++ Loop_.py
@@ -0,0 +1,14 @@
+# i = 0
+
+# while i < 10:
+#     print((2 * i))
+#     i = i + 1
+
+# for i in range(6):
+#     print(i)
+
+# else: print('Loop is finish')
+
+num = int(input('Enter a number: '))
+for i in range(1, 11):
+    print(str(num) + " X " + str(i) + " = " + str(num * i))
---
+++ in_is.py
@@ -0,0 +1,10 @@
+# a = None
+
+# if (a is None):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+a = [45, 56, 85, 75]
+print(4 in a)
